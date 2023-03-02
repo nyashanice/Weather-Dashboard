8ee8954495633136cc9dbe6ff50aa6d9b0fe2092
Nya Tyus <nyastyus@gmail.com>
2023-03-01 23:19:00 -0500

    use user input to get city weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
+// all variables used
+var city;
+var coordinates = [];
+var search = document.getElementById("search-bar");
+var button = document.getElementById("search-button");
+
+var currentWeather = document.getElementById('current-weather');
+
+// base url
 var requestUrl =
   "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e47eb086d90f9e94737e6cf64acdd28a";
 
 // search for city
 
-var requestCity =
-  "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=47eb086d90f9e94737e6cf64acdd28a";
-var getCoordinates =
-  "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=47eb086d90f9e94737e6cf64acdd28a";
 // show current and future conditions
 // add to seach history
 
@@ -14,10 +19,20 @@ var getCoordinates =
 // future weather shows 5 day forecast with dates, icon, temp, wind speed, humidity
 
 // click on search history to be taken back
-var city = "Atlanta";
-var lat;
-var lon;
 
+// button.addEventListener("click", input);
+
+// function input() {
+//   console.log(search.value);
+// }
+
+search.addEventListener("input", (e) => {
+  city = e.target.value;
+  console.log(city);
+  button.addEventListener("click", getCity(city));
+});
+
+// retrieves latitude and longitude from city requested
 function getCity(city) {
   fetch(
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -29,10 +44,12 @@ function getCity(city) {
     })
     .then(function (data) {
       console.log(data);
-      lat = data[0].lat;
-      lon = data[0].lon;
-      console.log(lat, lon);
+      //   lat = data[0].lat;
+      //   lat.push(data[0].lat);
+      //   lon.push(data[0].lon);
+      coordinates.push(data[0].lat, data[0].lon);
     });
 }
 
-getCity(city);
+// getCity(city);
+console.log(coordinates);
